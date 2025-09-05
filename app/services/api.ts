296474fbb455e74c1b73f996d9b20865edf6a2bf
LucasBenini1 <lucasbenini.11@gmail.com>
2025-09-05 15:09:22 -0300

    Troca de URL no api.ts

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,5 @@
 const NEXT_PUBLIC_BLINK_BE_BASE_URL=process.env.NEXT_PUBLIC_BLINK_BE_BASE_URL
 const NEXT_PUBLIC_BLINK_BE_BASE_URL_DOCK=process.env.NEXT_PUBLIC_BLINK_BE_BASE_URL_DOCK
-console.log("URL:" + NEXT_PUBLIC_BLINK_BE_BASE_URL_DOCK)
-console.log("URL 2:" + process.env.BLINK_BE_BASE_URL_DOCK)
 
 export const createApiHeaders = (token: string) => ({
   'Content-Type': 'application/json',
@@ -17,7 +15,7 @@ export const apiEndpoints = {
   appointmentsConfig: `${NEXT_PUBLIC_BLINK_BE_BASE_URL}/configurations/appointments`,
   exceptions: `${NEXT_PUBLIC_BLINK_BE_BASE_URL}/configurations/availability/1/exception`,
   exceptionsConfig: `${NEXT_PUBLIC_BLINK_BE_BASE_URL}/configurations/availability/exception`,
-  overview: `${NEXT_PUBLIC_BLINK_BE_BASE_URL_DOCK}/chat/1/overview`,
-  overviewPhone: `${NEXT_PUBLIC_BLINK_BE_BASE_URL_DOCK}/chat/1/overview`,
+  overview: `http://blink-be-dev:3003/api/v1/chat/1/overview`,
+  overviewPhone: `http://blink-be-dev:3003/api/v1/chat/1/overview`,
   dashboardInfo: `${NEXT_PUBLIC_BLINK_BE_BASE_URL}/dashboards/1`
 };
