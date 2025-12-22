a8ac20f251713d6c17b53e29d28d09ab4c2ba476
Matheus Leal <matheus.mleall@gmail.com>
2025-12-22 18:11:07 -0300

    AJUISTE DE ENV

--- app/api/ws-config/route.ts
+++ app/api/ws-config/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    wsUrl: process.env.NEXT_PUBLIC_BLINK_BE_WS,
+    wsUrl: process.env.BLINK_BE_PUBLIC_WS_URL,
   });
 }
