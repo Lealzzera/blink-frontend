c621a3ae2cf6156eaa82b9cf185a9d66e0ff381d
Matheus Leal <matheus.mleall@gmail.com>
2025-12-22 16:27:11 -0300

    removed useless layout

--- app/clientLayout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ReactNode } from "react";
-import { MyProvider } from "./context/context";
-
-export default function ClientLayout({ children }: { children: ReactNode }) {
-  return <MyProvider>{children}</MyProvider>;
-}
