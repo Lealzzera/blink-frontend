4c925b4105405beeae4251ee381afe3ebca82568
Matheus <matheus.mleall@gmail.com>
2026-05-16 10:41:52 -0300

    fixed login request

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -10,30 +10,37 @@ type LoginData = {
 
 export async function login({ email, password }: LoginData) {
   try {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_BLINK_BE_BASE_URL}/auth/login`,
-      { email, password },
-    );
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_BLINK_BE_BASE_URL}/auth/login`, {
+      email,
+      password,
+    });
 
-    const { accessToken, refreshToken } = response.data;
+    const { accessToken } = response.data;
 
     const cookieStore = await cookies();
 
     cookieStore.set('access_token', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      maxAge: 60 * 60 * 24, // 1 dia (alinhado com a expiração do backend)
+      maxAge: 60 * 60 * 24,
       path: '/',
       sameSite: 'lax',
     });
 
-    cookieStore.set('refresh_token', refreshToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 60 * 60 * 24 * 7, // 7 dias
-      path: '/',
-      sameSite: 'lax',
-    });
+    const cookieHeader = response.headers['set-cookie'];
+
+    if (cookieHeader?.length) {
+      const refreshTokenCookie = cookieHeader[0];
+      const refreshTokenValue = refreshTokenCookie.split(';')[0].split('=')[1];
+
+      cookieStore.set('refresh_token', refreshTokenValue, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 14,
+        path: '/',
+        sameSite: 'lax',
+      });
+    }
 
     return { error: null };
   } catch {
