27a7b21c20476f83f962b0cbdd0e897190845f2c
Matheus <matheus.mleall@gmail.com>
2026-05-17 00:13:01 -0300

    feat: updated the fetchChatOverview method

--- app/actions/fetchChatOverview.ts
+++ app/actions/fetchChatOverview.ts
@@ -1,7 +1,6 @@
 'use server';
 
-import axios from 'axios';
-import { cookies } from 'next/headers';
+import { serverApi } from './serverApi';
 
 type GetConversationsType = {
   pagination?: {
@@ -14,31 +13,14 @@ type GetConversationsType = {
 const PAGE_SIZE = 20;
 
 export async function fetchChatOverview({ pagination, sessionName }: GetConversationsType) {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get('access_token')?.value;
-
-  if (!accessToken) throw new Error('User is not authenticated');
-
-  try {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_BLINK_BE_BASE_URL}/whatsapp/chats/${sessionName}/overview`,
-      {
-        pagination: {
-          limit: pagination?.limit ?? PAGE_SIZE,
-          offset: pagination?.offset ?? 0,
-        },
+  return await serverApi({
+    method: 'POST',
+    url: `/whatsapp/chats/${sessionName}/overview`,
+    data: {
+      pagination: {
+        limit: pagination?.limit ?? PAGE_SIZE,
+        offset: pagination?.offset ?? 0,
       },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
-        },
-      },
-    );
-
-    return response.data;
-  } catch (err) {
-    console.error('Error fetching conversations:', err);
-    return [];
-  }
+    },
+  });
 }
