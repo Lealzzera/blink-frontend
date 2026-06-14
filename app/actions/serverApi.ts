55f07062f0d2b1e461d1421972714682b609fe1c
Matheus <matheus.mleall@gmail.com>
2026-06-14 11:42:39 -0300

    added an error handling to handle problems with refresh and access token

--- app/actions/serverApi.ts
+++ app/actions/serverApi.ts
@@ -2,6 +2,7 @@
 
 import axios, { AxiosRequestConfig } from 'axios';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export async function serverApi(config: AxiosRequestConfig) {
   const cookieStore = await cookies();
@@ -20,26 +21,39 @@ export async function serverApi(config: AxiosRequestConfig) {
     return response.data;
   } catch (err: any) {
     if (err.response?.status !== 401) {
-      throw err;
+      console.error('Unexpected error:', err);
+      cookieStore.delete('access_token');
+      cookieStore.delete('refresh_token');
+      redirect('/');
     }
 
     const refreshToken = cookieStore.get('refresh_token')?.value;
 
     if (!refreshToken) {
-      throw new Error('Refresh token missing');
+      console.error('Refresh token missing');
+      cookieStore.delete('access_token');
+      cookieStore.delete('refresh_token');
+      redirect('/');
     }
 
-    const refreshResponse = await axios.post(
-      `${process.env.NEXT_PUBLIC_BLINK_BE_BASE_URL}/auth/refresh`,
-      {},
-      {
-        headers: {
-          Cookie: `refresh_token=${refreshToken}`,
+    const refreshResponse = await axios
+      .post(
+        `${process.env.NEXT_PUBLIC_BLINK_BE_BASE_URL}/auth/refresh`,
+        {},
+        {
+          headers: {
+            Cookie: `refresh_token=${refreshToken}`,
+          },
         },
-      },
-    );
+      )
+      .catch((err) => {
+        console.error('Invalid refresh token', err);
+        cookieStore.delete('access_token');
+        cookieStore.delete('refresh_token');
+        redirect('/');
+      });
 
-    const newAccessToken = refreshResponse.data.access_token;
+    const newAccessToken = refreshResponse?.data?.access_token;
 
     cookieStore.set('access_token', newAccessToken, {
       httpOnly: true,
@@ -49,7 +63,7 @@ export async function serverApi(config: AxiosRequestConfig) {
       maxAge: 60 * 60 * 24 * 14,
     });
 
-    const setCookie = refreshResponse.headers['set-cookie'];
+    const setCookie = refreshResponse?.headers['set-cookie'];
 
     if (setCookie?.length) {
       const refreshCookie = setCookie.find((cookie: string) => cookie.startsWith('refresh_token='));
